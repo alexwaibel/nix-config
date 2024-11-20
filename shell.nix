cf9ed26afd6aacbd7b72418f9da0e37ccfe3a9e7
Alex Waibel <alexwaibel@users.noreply.github.com>
2024-11-19 19:35:49 -0800

    Add shell.nix and editorconfig

---
+++ shell.nix
@@ -0,0 +1,10 @@
+{pkgs ? import <nixpkgs> {}, ...}: {
+  default = pkgs.mkShell {
+    NIX_CONFIG = "extra-experimental-features = nix-command flakes repl-flake";
+    nativeBuildInputs = with pkgs; [
+      nix
+      home-manager
+      git
+    ];
+  };
+}
