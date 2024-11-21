f1097efb5210d9a28e53d7c2a4023466a3965388
Alex Waibel <alexwaibel@users.noreply.github.com>
2024-11-20 22:15:22 -0800

    No trailing commas needed in array

--- home/alex/features/cli/nvim/default.nix
+++ home/alex/features/cli/nvim/default.nix
@@ -84,38 +84,40 @@
         mode = "n";
         key = "<Esc>";
         action = "<cmd>nohlsearch<CR>";
-      },
+      }
+
       # Diagnostic keymaps
       {
         mode = "n";
         key = "[d";
         options.desc = "Go to previous [D]iagnostic message"
         action = vim.diagnostic.goto_prev;
-      },
+      }
       {
         mode = "n";
         key = "]d";
         options.desc = "Go to next [D]iagnostic message"
         action = vim.diagnostic.goto_next;
-      },
+      }
       {
         mode = "n";
         key = "<leader>e";
         options.desc = "Show diagnostic [E]rror messages"
         action = vim.diagnostic.open_float;
-      },
+      }
       {
         mode = "n";
         key = "<leader>q";
         options.desc = "Open diagnostic [Q]uickfix list"
         action = vim.diagnostic.setloclist;
-      },
+      }
       {
         mode = "n";
         key = "<leader>q";
         options.desc = "Open diagnostic [Q]uickfix list"
         action = vim.diagnostic.setloclist;
-      },
+      }
+
       # Exit terminal mode in builtin terminal with double escape. Easier to discover than default <C-\><C-n>
       # Won't work in all terminal emulators/tmux/etc
       {
@@ -123,26 +125,27 @@
         key = "<Esc><Esc>";
         options.desc = "Exit terminal mode"
         action = "<C-\\><C-n>";
-      },
+      }
+
       # Make split navigation Easier
       {
         mode = "n";
         key = "<C-h>";
         options.desc = "Move focus to the left window"
         action = "<C-w><C-h>";
-      },
+      }
       {
         mode = "n";
         key = "<C-l>";
         options.desc = "Move focus to the right window"
         action = "<C-w><C-l>";
-      },
+      }
       {
         mode = "n";
         key = "<C-j>";
         options.desc = "Move focus to the lower window"
         action = "<C-w><C-j>";
-      },
+      }
       {
         mode = "n";
         key = "<C-k>";
