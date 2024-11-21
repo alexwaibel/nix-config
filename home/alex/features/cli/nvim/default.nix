1f98f93f5353af85bf87f8466aefa6fb24e840f5
Alex Waibel <alexwaibel@users.noreply.github.com>
2024-11-20 22:16:45 -0800

    Add missing semicolons

--- home/alex/features/cli/nvim/default.nix
+++ home/alex/features/cli/nvim/default.nix
@@ -90,31 +90,31 @@
       {
         mode = "n";
         key = "[d";
-        options.desc = "Go to previous [D]iagnostic message"
+        options.desc = "Go to previous [D]iagnostic message";
         action = vim.diagnostic.goto_prev;
       }
       {
         mode = "n";
         key = "]d";
-        options.desc = "Go to next [D]iagnostic message"
+        options.desc = "Go to next [D]iagnostic message";
         action = vim.diagnostic.goto_next;
       }
       {
         mode = "n";
         key = "<leader>e";
-        options.desc = "Show diagnostic [E]rror messages"
+        options.desc = "Show diagnostic [E]rror messages";
         action = vim.diagnostic.open_float;
       }
       {
         mode = "n";
         key = "<leader>q";
-        options.desc = "Open diagnostic [Q]uickfix list"
+        options.desc = "Open diagnostic [Q]uickfix list";
         action = vim.diagnostic.setloclist;
       }
       {
         mode = "n";
         key = "<leader>q";
-        options.desc = "Open diagnostic [Q]uickfix list"
+        options.desc = "Open diagnostic [Q]uickfix list";
         action = vim.diagnostic.setloclist;
       }
 
@@ -123,7 +123,7 @@
       {
         mode = "t";
         key = "<Esc><Esc>";
-        options.desc = "Exit terminal mode"
+        options.desc = "Exit terminal mode";
         action = "<C-\\><C-n>";
       }
 
@@ -131,25 +131,25 @@
       {
         mode = "n";
         key = "<C-h>";
-        options.desc = "Move focus to the left window"
+        options.desc = "Move focus to the left window";
         action = "<C-w><C-h>";
       }
       {
         mode = "n";
         key = "<C-l>";
-        options.desc = "Move focus to the right window"
+        options.desc = "Move focus to the right window";
         action = "<C-w><C-l>";
       }
       {
         mode = "n";
         key = "<C-j>";
-        options.desc = "Move focus to the lower window"
+        options.desc = "Move focus to the lower window";
         action = "<C-w><C-j>";
       }
       {
         mode = "n";
         key = "<C-k>";
-        options.desc = "Move focus to the upper window"
+        options.desc = "Move focus to the upper window";
         action = "<C-w><C-k>";
       }
     ];
