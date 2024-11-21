0156bec58b3fea64ac14c1520c62f722e5501c1e
Alex Waibel <alexwaibel@users.noreply.github.com>
2024-11-20 22:41:13 -0800

    Fix nixvim branch config

--- flake.nix
+++ flake.nix
@@ -13,8 +13,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nixvim = {
-      url = "github:nix-community/nixvim";
-      ref = "nixos-24.05";
+      url = "github:nix-community/nixvim/nixos-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
