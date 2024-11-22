5b0f9aebc913eb8fee112bee681b855174f0efc9
Alex Waibel <alexwaibel@users.noreply.github.com>
2024-11-22 00:53:45 -0800

    Switch to optimise automatic

--- hosts/common/global/nix.nix
+++ hosts/common/global/nix.nix
@@ -8,8 +8,8 @@
         "nix-command"
         "flakes"
       ];
-      auto-optimise-store = true;
     };
+    optimise.automatic = true;
     gc = {
       automatic = true;
       ${if !darwin then "dates" else null} = "weekly";
