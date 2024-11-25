aecbf609fae766d353bb8282ceb77ff498f5b79b
Alex Waibel <alexwaibel@users.noreply.github.com>
2024-11-24 18:56:45 -0800

    Only install linux specific packages when host platform is linux

--- home/alex/features/cli/default.nix
+++ home/alex/features/cli/default.nix
@@ -57,19 +57,18 @@
     glow # markdown previewer in terminal
 
     btop  # replacement of htop/nmon
-    iotop # io monitoring
     iftop # network monitoring
-
-    # system call monitoring
+    lsof # list open files
+    pciutils # lspci
+  ] ++ (pkgs.lib.optionals pkgs.stdenv.isLinux [
+    iotop # io monitoring
     strace # system call monitoring
     ltrace # library call monitoring
-    lsof # list open files
 
     # system tools
     sysstat
     lm_sensors # for `sensors` command
     ethtool
-    pciutils # lspci
     usbutils # lsusb
-  ];
+  ]);
 }
